31c36e328eb797c3e2c9cb86e7048ee404c7099c
Kelvin <kmunyimbili@gmail.com>
2023-10-10 07:48:43 +0200

    CHANGE BG IMAGE AND STYLE BUTTONS

--- main.js
+++
@@ -1,7 +0,0 @@
-const root = document.documentElement;
-
-document.querySelector('button').addEventListener('click', () => {
-    const newTheme = root.className === 'light'? 'dark' : 'light';
-    root.className = newTheme;
-    root.querySelector('button').className = newTheme;
-})
